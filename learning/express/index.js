4323556220b23ed3c8265feeab74fdbf00907b23
ParasJain1803 <paras2004jain@gmail.com>
2025-01-23 11:42:21 +0530

    folder name

--- learning/express/index.js
+++ learning/express/index.js
@@ -6,3 +6,8 @@ let port = 3000;
 app.listen(port, () => {
     console.log(`app is listening at http://localhost:${port}`);
 })
+
+app.use((req, res) => {
+    console.log(`Request received}`);
+    res.send("Hello, world!");
+})
