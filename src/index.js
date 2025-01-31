473d471bea84b7d8288855bce878eecf2cd9f21a
ParasJain1803 <paras2004jain@gmail.com>
2025-01-31 19:02:29 +0530

    utils and error handling

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 // require('dotenv').config({path: './env'})
-import dotenv from "dotenv";
-
 import connectDB from "./db/index.js";
-
+import dotenv from "dotenv";
 dotenv.config({ path: "./env" });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.error(error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  })
+  .cathch((error) => {
+    console.log("MongoDB connection failed !!! ", error);
+  });
 
 /*
 import express from "express";
