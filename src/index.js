5b04b9fdea69f953447c9bf61c25ad164c6f1d62
ParasJain1803 <paras2004jain@gmail.com>
2025-02-01 21:51:55 +0530

    router and controller

--- src/index.js
+++ src/index.js
@@ -1,26 +1,20 @@
 // require('dotenv').config({path: './env'})
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
+import {app} from "./app.js"
 dotenv.config({ path: "./env" });
 
 connectDB()
   .then(() => {
-    app.on("error", (error) => {
-      console.error(error);
-      throw error;
-    });
-
     app.listen(process.env.PORT || 8000, () => {
       console.log(`Server is running on port ${process.env.PORT}`);
     });
   })
-  .cathch((error) => {
+  .catch((error) => {
     console.log("MongoDB connection failed !!! ", error);
   });
 
 /*
-import express from "express";
-const app = express();
 
 (async () => {
   try {
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,12 +1,24 @@
-const asyncHandler = (fn) => async (req, res, next) => {
-  try {
-    await fn(req, res, next);
-  } catch (error) {
-    res.status(error.code || 500).json({
-      success: false,
-      message: error.message || "Server Error",
-    });
+const asyncHandler = (requestHandler) => {
+  return (req, res, next) => {
+      Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
   }
-};
+}
 
-export { asyncHandler };
+
+export { asyncHandler }
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//   try {
+//     console.log("in asyncHandler");
+
+//     return await fn(req, res, next);
+//   } catch (error) {
+//     res.status(error.code || 500).json({
+//       success: false,
+//       message: error.message || "Server Error",
+//     });
+//   }
+// };
+
+// export { asyncHandler };
+
